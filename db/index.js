40671418af2fe5b267125a9a3f518662ad464858
k-zammito <kzammito@gmail.com>
2021-11-18 19:51:18 -0500

    added data and server syncandseed, need to add delete still

---
+++ db/index.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const faker = require('faker')
+const { STRING, TEXT } = Sequelize;
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_users_db');
+
+const User = db.define('User', { // MODELS
+    email: {
+        type: STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    }, 
+    bio: {
+        type: TEXT
+    }
+});
+
+User.beforeSave(user => {
+    if (!user.bio) {
+        user.bio = `${user.email} BIO is ${faker.lorem.paragraphs(3)}!`
+    }
+})
+
+const syncAndSeed = async() => {
+    await db.sync({ force:true }); // drops/adds new tables if they already
+    await User.create({ email: 'moe@gmail.com', bio: 'This is the bio for Moe'}) // creates new users with specified field
+    await User.create({ email: 'lucy@yahoo.com'})
+    await User.create({ email: 'ethyl@aol.com'})
+};
+ 
+module.exports = {
+    db,
+    syncAndSeed,
+    models: {
+        User
+    }
+}
--- server.js
+++ server.js
@@ -1,41 +1,103 @@
-const Sequelize = require('sequelize');
-const faker = require('faker')
-const { STRING, TEXT } = Sequelize;
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_users_db');
+const { db, syncAndSeed, models: { User } } = require('./db')
+const express = require('express')
+const path = require('path')
+const app = express();
 
-const User = db.define('User', { // MODELS
-    email: {
-        type: STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
-    }, 
-    bio: {
-        type: TEXT
+app.use(express.urlencoded({ extended:false })) // use for forms 
+
+app.get('/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'styles.css')))
+
+app.get('/', (req, res) => res.redirect('/users')); // redicted link to /users
+
+app.delete('/users/:foo', async(req, res, next) => { // need to add button for funtionality... & methodoverride from POST to DELETE...
+    try {
+        const user = await User.findByPk(req.params.foo)
+        await user.destroy();
+        res.redirect('/users')
     }
-});
-
-User.beforeSave(user => {
-    if (!user.bio) {
-        user.bio = `${user.email} BIO is ${faker.lorem.paragraphs(3)}!`
+    catch(ex) {
+        next(ex)
     }
 })
 
-const syncAndSeed = async() => {
-    await db.sync({ force:true }); // drops/adds new tables if they already
-    await User.create({ email: 'moe@gmail.com', bio: 'This is the bio for Moe'}) // creates new users with specified field
-    await User.create({ email: 'lucy@yahoo.com'})
-    await User.create({ email: 'ethyl@aol.com'})
-};
+app.post('/users', async(req, res, next) => {
+    try {
+        const user = await User.create(req.body)
+        res.redirect(`/users/${user.id}`)
+    }
+    catch(ex) {
+        next(ex)
+    }
+})
+
+app.get('/users', async(req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.send(`
+        <html>
+            <head>
+                <link rel='stylesheet' href='/styles.css'/>
+            </head>
+            <body>
+                <h1>Users (${ users.length }) </h1>
+                <form method='POST' id='user-form'> 
+                    <input name='email' placeHolder='enter email'/>
+                    <textarea name='bio'></textarea>
+                    <button>Create User</button>
+                </form>
+                <ul>
+                    ${ users.map( user => `
+                    <li>
+                        <a href='/users/${user.id}'>
+                        ${ user.email }
+                        </a>
+                    </li>
+                    `).join('')}
+                </ul>
+            </body>
+        </html>
+        `);
+    }
+    catch(ex) {
+        next(ex);
+    }
+});
+
+app.get('/users/:foo', async(req, res, next) => {
+    try {
+        const user = await User.findByPk(req.params.foo);
+        const users = await User.findAll();
+        res.send(`
+        <html>
+            <head>
+                <link rel='stylesheet' href='/styles.css'/>
+            </head>
+            <body>
+                <h1>Users ${ users.length } </h1>
+                <a href='/users/'>
+                ${ user.email }
+                </a>
+                <p>
+                ${ user.bio }
+                </p>
+            </body>
+        </html>
+        `);
+    }
+    catch(ex) {
+        next(ex);
+    }
+});
 
 const init = async() => {    
     try { 
         await syncAndSeed()
+        const port = process.env.PORT || 3000;
+        app.listen(port, ()=> console.log(`listening on port ${port}`))
     }
     catch(ex) {
         console.log(ex);
     }
 }
 
-init();
+init(); 
